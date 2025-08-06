74541d2727ccfab52e47cad5fadebfc73584791f
sistemas10info <sistemas10.info@gmail.com>
2025-08-06 11:48:09 -0300

    teste upload

--- painel_admin/vendedores.php
+++ painel_admin/vendedores.php
@@ -28,6 +28,55 @@ if (file_exists($arquivo)) {
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
+		<!-- Font Awesome -->
+		<link href="../bootstrap/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+		<!-- NProgress -->
+		<link href="../bootstrap/vendors/nprogress/nprogress.css" rel="stylesheet">
+		<!-- iCheck -->
+		<link href="../bootstrap/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+		<!-- bootstrap-daterangepicker -->
+		<link href="../bootstrap/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+		<!-- bootstrap-datetimepicker -->
+		<link href="../bootstrap/vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+		<!-- Bootstrap Colorpicker -->
+		<link href="../bootstrap/vendors/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css" rel="stylesheet">
+
+		<!-- bootstrap-progressbar -->
+		<link href="../bootstrap/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+		<!-- JQVMap -->
+		<link href="../bootstrap/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+
+		<!-- Switchery -->
+		<link href="../bootstrap/vendors/switchery/dist/switchery.min.css" rel="stylesheet">
+
+		<link href="../bootstrap/assets/plugins/bootgrid/jquery.bootgrid.min.css" rel="stylesheet"> 
+		<link href="../bootstrap/assets/plugins/lightbox/css/lightbox.css" rel="stylesheet" />
+		<link href="../bootstrap/assets/plugins/summernote/summernote.css" rel="stylesheet">
+
+		<!-- Dropzone -->
+		<link href="../bootstrap/assets/plugins/dropzone/min/dropzone.min.css" rel="stylesheet" />
+
+		<!-- PNotify -->
+		<link href="../bootstrap/vendors/pnotify/dist/pnotify.css" rel="stylesheet">
+		<link href="../bootstrap/vendors/pnotify/dist/pnotify.buttons.css" rel="stylesheet">
+		<link href="../bootstrap/vendors/pnotify/dist/pnotify.nonblock.css" rel="stylesheet">
+		
+		<!-- Custom Theme Style -->
+		<!-- <link href="../../build/css/custom.min.css" rel="stylesheet"> -->
+		
+		<!-- SELECT BOOTSTRAP -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+
+	    <!-- Bootstrap core JavaScript-->
+	    <script src="vendor/jquery/jquery.min.js"></script>
+	    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	
+	    <!-- Core plugin JavaScript-->
+	    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+	
+	    <!-- Custom scripts for all pages-->
+	    <script src="js/sb-admin-2.min.js"></script>
+
 </head>
 
 <body id="page-top"> 
@@ -117,6 +166,40 @@ if (file_exists($arquivo)) {
         <i class="fas fa-angle-up"></i>
     </a>
 
+		<!-- FastClick -->
+		<script src="../bootstrap/vendors/fastclick/lib/fastclick.js"></script>
+		<!-- NProgress -->
+		<script src="../bootstrap/vendors/nprogress/nprogress.js"></script>
+		<!-- iCheck -->
+		<script src="../bootstrap/vendors/iCheck/icheck.min.js"></script>
+		<!-- bootstrap-daterangepicker -->
+		<script src="../bootstrap/vendors/moment/min/moment.min.js"></script>
+		<script src="../bootstrap/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+		<!-- bootstrap-datetimepicker -->    
+		<script src="../bootstrap/vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+		<!-- Bootstrap Colorpicker -->
+		<script src="../bootstrap/vendors/mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
+		<!-- PNotify -->
+		<script src="../bootstrap/vendors/pnotify/dist/pnotify.js"></script>
+		<script src="../bootstrap/vendors/pnotify/dist/pnotify.buttons.js"></script>
+		<script src="../bootstrap/vendors/pnotify/dist/pnotify.nonblock.js"></script>
+		<!-- Switchery -->
+		<script src="../bootstrap/vendors/switchery/dist/switchery.min.js"></script>
+		<!-- jquery.inputmask -->
+		<script src="../bootstrap/vendors/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js"></script>
+		
+		<!-- Custom Theme Scripts -->
+		<script src="../../build/js/custom.min.js"></script>
+
+	<script src="../bootstrap/assets/plugins/shortenerUrl/jquery.urlshortener.js"></script>
+	<script src="../bootstrap/assets/plugins/bootgrid/jquery.bootgrid.min.js"></script>
+	<script src="../bootstrap/assets/plugins/bootgrid/jquery.bootgrid.fa.js" type="text/javascript"></script>
+	<script src="../bootstrap/assets/plugins/eModal/dist/eModal.js"></script>
+	<script src="../bootstrap/assets/plugins/jquery-maskmoney/jquery.maskMoney.min.js"></script>
+	<script src="../bootstrap/assets/plugins/lightbox/js/lightbox.min.js"></script>
+	<script src="../bootstrap/assets/plugins/summernote/summernote.min.js"></script>
+	<script src="../bootstrap/assets/plugins/dropzone/min/dropzone.min.js"></script>
+
 	 <script src="../globais/admin/js/pages/vendedores.js">
 
     <!-- Bootstrap core JavaScript-->
@@ -129,8 +212,6 @@ if (file_exists($arquivo)) {
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
 
-    
-   
 
 </body>
 
